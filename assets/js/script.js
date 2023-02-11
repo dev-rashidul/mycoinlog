c7cd68975fa0578e6d20b367a856e52e8158881d
dev-rashidul <rashidul.contact@gmail.com>
2023-02-11 13:57:48 +0600

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,22 @@
 // Sticky Navbar for Header
 
-window.addEventListener("scroll", function(){
-  var header = document.querySelector(".navbar")
-  header.classList.toggle("sticky", window.scrollY > 0)
-})
+window.addEventListener("scroll", function () {
+  var header = document.querySelector(".navbar");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
 // Dashboard Slider JavaScript
 
-$(".dashboard-slider").slick({
-  dots: false,
-  arrows: false,
-  infinite: true,
-  speed: 300,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  slidesToShow: 1,
-  centerMode: true,
-  variableWidth: true,
+jQuery(function ($) {
+  $(".dashboard-slider").slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+  });
 });
