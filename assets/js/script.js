57c2bafc6f2a70bafca5512e2be6eb358c580cc8
dev-rashidul <rashidul.contact@gmail.com>
2023-02-12 16:42:31 +0600

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,3 @@ window.addEventListener("scroll", function () {
   var header = document.querySelector(".navbar");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
-
-// Dashboard Slider JavaScript
-
-jQuery(function ($) {
-  $(".dashboard-slider").slick({
-    dots: false,
-    arrows: false,
-    infinite: true,
-    speed: 300,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true,
-  });
-});
